95badf4d027d9184e7ce56595c369b468fac2060
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-01-13 15:19:23 -0300

    Add files via upload

---
+++ conditional_statements_advanced_lab/fruit_shop_revision.py
@@ -0,0 +1,51 @@
+fruit = input()
+day = input()
+quantity = float(input())
+price = 0
+error = None
+
+if day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday":
+    if fruit == "banana":
+        price = 2.5
+    elif fruit == "apple":
+        price = 1.2
+    elif fruit == "orange":
+        price = 0.85
+    elif fruit == "grapefruit":
+        price = 1.45
+    elif fruit == "kiwi":
+        price = 2.7
+    elif fruit == "pineapple":
+        price = 5.5
+    elif fruit == "grapes":
+        price = 3.85
+    else:
+        error = True
+        print("error")
+
+elif day == "Saturday" or day == "Sunday":
+    if fruit == "banana":
+        price = 2.7
+    elif fruit == "apple":
+        price = 1.25
+    elif fruit == "orange":
+        price = 0.9
+    elif fruit == "grapefruit":
+        price = 1.60
+    elif fruit == "kiwi":
+        price = 3
+    elif fruit == "pineapple":
+        price = 5.6
+    elif fruit == "grapes":
+        price = 4.2
+    else:
+        error = True
+        print("error")
+
+else:
+    error = True
+    print("error")
+
+fruit_cost = quantity * price
+if not error:
+    print(f"{fruit_cost:.2f}")
