7a2f7e87334f4c5051a15cf87bb7eb779430dce5
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-01-14 15:50:44 -0300

    Add files via upload

---
+++ conditional_statements_advanced_exercise/journey_revision.py
@@ -0,0 +1,36 @@
+budget = float(input())
+season = input()
+
+destination = ""
+place = ""
+price = 0
+
+if budget <= 100:
+    destination = "Bulgaria"
+
+    if season == "summer":
+        price = budget * 0.3
+        place = "Camp"
+
+    elif season == "winter":
+        price = budget * 0.7
+        place = "Hotel"
+
+elif 100 < budget <= 1000:
+    destination = "Balkans"
+
+    if season == "summer":
+        price = budget * 0.4
+        place = "Camp"
+
+    elif season == "winter":
+        price = budget * 0.8
+        place = "Hotel"
+
+elif budget > 1000:
+    destination = "Europe"
+    price = budget * 0.9
+    place = "Hotel"
+
+print(f"Somewhere in {destination}")
+print(f"{place} - {price:.2f}")
