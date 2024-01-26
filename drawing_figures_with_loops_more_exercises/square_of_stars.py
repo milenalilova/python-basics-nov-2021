d61c7cebc1d7427dec63169d7e463e2ef48417f9
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-01-26 16:07:33 -0300

    Add files via upload

---
+++ drawing_figures_with_loops_more_exercises/square_of_stars.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    print(n * "* ")
+
+
+
+# Variant 2
+# count = int(input())
+# for i in range(count):
+#     print("*", end="")
+#     for j in range(count - 1):
+#         print(" *", end="")
+#     print()
