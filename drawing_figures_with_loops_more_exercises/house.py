040c8c5d864dc6c6fe6fff7cc13573523c0ee18b
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-01-29 15:42:08 -0300

    Add files via upload

--- drawing_figures_with_loops_more_exercises/house.py
+++ drawing_figures_with_loops_more_exercises/house.py
@@ -18,3 +18,23 @@ for i in range((n + 1) // 2):
 
 for j in range(n // 2):
     print(f"|{(n - 2) * '*'}|")
+
+
+
+# Variant 2
+# count = int(input())
+#
+# for i in range((count + 1) // 2):
+#     if i == 0:
+#         if count % 2 == 0:
+#             print(f"{'-' * ((count - 2) // 2)}**{'-' * ((count - 2) // 2)}")
+#         else:
+#             print(f"{'-' * ((count - 1) // 2)}*{'-' * ((count - 1) // 2)}")
+#     else:
+#         if count % 2 == 0:
+#             print(f"{'-' * ((count - (i+1) * 2) // 2)}{'*' * ((i+ 1) * 2)}{'-' * ((count - (i+1) * 2) // 2)}")
+#         else:
+#             print(f"{'-' * ((count + 1 - (i+1) * 2) // 2)}{'*' * ((i+ 1) * 2 - 1)}{'-' * ((count + 1 - (i+1) * 2) // 2)}")
+#
+# for i in range (count - (count + 1) // 2):
+#     print(f"|{'*' * (count - 2)}|")
