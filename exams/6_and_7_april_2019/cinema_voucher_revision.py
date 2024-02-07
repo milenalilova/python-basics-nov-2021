1c24298798721f1ef03d3023621a88dc562475a2
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-07 11:42:27 -0300

    Add files via upload

--- exams/6_and_7_april_2019/cinema_voucher_revision.py
+++ exams/6_and_7_april_2019/cinema_voucher_revision.py
@@ -1,1 +1,31 @@
-vaucher_value=int(input())
+voucher_value = int(input())
+
+tickets_bought = 0
+other_purchases_bought = 0
+
+while voucher_value > 0:
+    command = input()
+    if command == "End":
+        break
+    purchase_value = 0
+
+    if len(command) > 8:
+        purchase_value += ord(command[0]) + ord(command[1])
+        if purchase_value <= voucher_value:
+            voucher_value -= purchase_value
+            tickets_bought += 1
+
+        else:
+            break
+
+    else:
+        purchase_value += ord(command[0])
+        if purchase_value <= voucher_value:
+            voucher_value -= purchase_value
+            other_purchases_bought += 1
+
+        else:
+            break
+
+print(f"{tickets_bought}")
+print(f"{other_purchases_bought}")
